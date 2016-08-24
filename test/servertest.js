5da0a968d5319be706977e56e6fcfd2496aada49
Andre Rocha <devel.andrerocha@gmail.com>
2016-08-24 18:01:59 -0300

    add promise

--- test/servertest.js
+++ test/servertest.js
@@ -1,19 +1,24 @@
 var expect  = require("chai").expect;
 var request = require("request");
+var promise = require("promise");
 
 describe("Calc API test", function(){
   describe("Sum test for code", function(){
     var url =  "http://localhost:3000/sum?numberA=1&numberB=2";
     it("status cod should be 200", function(){
       request(url, function(error, response, body){
-        expect(response.code).to.equal(200);
-        done();
+        return new Promise(function(fulfill, reject){
+          expect(response.code).to.equal(200);
+          done();
+        });
       });
     });
     it("response body should be 3", function(){
       request(url, function(error, response, body){
-        expect(response.body).to.equal(JSON.stringify('3'));
-        done();
+        return new Promise(function(fulfill, reject){
+          expect(response.body).to.equal(JSON.stringify('3'));
+          done();
+        });
       });
     })
   });
